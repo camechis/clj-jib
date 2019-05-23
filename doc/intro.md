cf474f7c93391de7fb17be490cbde6048ab049b5
Travis Camechis <tcamechis@centripetalnetworks.com>
2019-05-23 11:43:51 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-jib
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/camechis/clj_jib_test.clj
@@ -0,0 +1,7 @@
+(ns camechis.clj-jib-test
+  (:require [clojure.test :refer :all]
+            [camechis.clj-jib :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
